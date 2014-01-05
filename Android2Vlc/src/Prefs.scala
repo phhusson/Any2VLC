70886d2f544a2306f29d87c1db25a88673a04a41
Pierre-Hugues Husson <pierre-hugues.husson@telecom-paristech.fr>
2014-01-05 01:22:21 +0100

    Fix Prefs...

--- Android2Vlc/src/Prefs.scala
+++ Android2Vlc/src/Prefs.scala
@@ -5,10 +5,19 @@ import android.content.{SharedPreferences,Context}
 
 class Prefs(preferences : SharedPreferences) extends Preferences(preferences) {
 	def server = super.server("192.168.1.1");
+	def server_=(s: String):Unit = super.server = s
+
 	def port = super.port("8080");
-	def proxy = super.port("/requests/status.xml");
+	def port_=(s: String):Unit = super.port = s
+
+	def proxy = super.proxy("/requests/status.xml");
+	def proxy_=(s: String):Unit = super.proxy = s
+
 	def password = super.password("azertyuiop");
-	def dlapi = super.password("http://youtube-dl.appspot.com/api/?url=");
+	def password_=(s: String):Unit = super.password = s
+
+	def dlapi = super.dlapi("http://youtube-dl.appspot.com/api/?url=");
+	def dlapi_=(s: String):Unit = super.dlapi = s
 }
 
 object Prefs {
