cec4df2ce0493a5fe3c4aba4d098a7d0766f8cc8
Pierre-Hugues Husson <husson@archos.com>
2014-03-26 10:03:48 +0100

    Youtube ? Wtf are you doing ?

--- Android2Vlc/src/Share.scala
+++ Android2Vlc/src/Share.scala
@@ -4,6 +4,7 @@ import org.scaloid.common._
 import scala.reflect.ClassTag
 import android.graphics.Color
 import android.content.{Context,Intent,IntentFilter,BroadcastReceiver}
+import scala.util.matching.Regex
 
 class Share extends SActivity {
 	val serv = new LocalServiceConnection[ShareService]
@@ -12,6 +13,9 @@ class Share extends SActivity {
 		var i : Intent = getIntent()
 		var action : String = i.getAction()
 		var uri : String = i.getStringExtra(Intent.EXTRA_TEXT)
+
+        var re = "http[^ ]*$".r
+        uri = (re findAllIn uri).mkString(" ")
 
 		serv.onConnected += {
 			s : ShareService => s.open(uri)
